bb6eaa5ce82067026cf577d252908b4aace96037
Giulio Golinelli <giulio.golinelli@studio.unibo.it>
2021-02-04 17:33:15 +0100

    added silent mode

--- script.sh
+++ script.sh
@@ -7,41 +7,61 @@ script is an utility script for the skyline project
 --> It runs the source with as many datafiles you want
 --> It compares results with the serial version
 
-Usage: ./script mode [datafiles..]
+Usage: ./script method [-s] [datafiles..]
 
-Mode is an interger that corresponds to the parallel technology used. In particular:
-	0: Open-mp
-	1: mpi
+Method:
+Method is a string that corresponds to the parallel technology used. In particular:
+	omp: Open-mp parallel technology
+	mpi: mpi parallel technology
 
+-s	--silence	suppress output execution of skyline algorithms	
+
+[datafiles..]
 In the datafiles.. argument you can specify which datafile you want to be tested.
 If no datafile is provided circle-N1000-D2.in datafile will be used by default.
 Example:
 	./script 0  /datafiles/circle-N1000-D2.in		(only one datafile)
 	./script 0 						(same as above)
 	./script 0 $(find ./datafiles/ -name /'test[1234]*/') 	(first four test datafiles)
-	./script 0 $(find ./datafiles/ -name /'*.in/') 		(all datafiles)
+	./script 0 -s $(find ./datafiles/ -name /'*.in/') 	(all datafiles, silent mode)
 
 All results are saved in ./results/
 '
 	exit 0
 fi
 
-if [[ $# -eq 0 || ( "$1" != "0" && "$1" != "1" ) ]]; then
+#check for mandatory argument
+if [[ $# -eq 0 || ( "$1" != "omp" && "$1" != "mpi" ) ]]; then
 	echo 'Not valid input. 
 Usage: ./script mode [datafiles..]. Use -h or --help to display the help message.'
 	exit 128
 fi
+mode=$1
+shift; #deleting mandatory argument from $@
 
+#check for silent mode
+if [[ $1 == "-s" || $1 == "--silence" ]]; then
+	exec 2>/dev/null
+	shift; #deleting -s optional argument from $@
+fi
+
+#creating /results/ folder if not already exists
+mkdir -p ./results/
+
+#loading datafiles
 datafiles=""
-
-if [[ $2 == "" ]]; then
+if [[ $1 == "" ]]; then
 	datafiles='./datafiles/circle-N1000-D2.in'
 else
-	for arg in "${@:2}"; do
-		if [[ $datafiles == "" ]]; then
-			datafiles=$arg
+	for datafile in "${@}"; do
+		if [[ -e $datafile ]]; then
+			if [[ $datafiles == "" ]]; then
+				datafiles=$datafile
+			else
+				datafiles="${datafiles} $datafile"
+			fi
 		else
-			datafiles="${datafiles} $arg"
+			echo "${datafile} doesn't  exits!"
 		fi
 	done
 fi
@@ -56,29 +76,20 @@ correct=0
 errors=0
 
 for datafile in $datafiles; do
-	if [[ -e $datafile ]]; then
-		echo "computing $datafile.."
-		temp="${datafile##*/}"
-		temp="${temp::-2}out"
-		serial="./results/serial-${temp}"
-		./src/skyline < $datafile > $serial
-		if [[ $1 -eq 0 ]]; then
-			parallel="./results/omp-${temp}"
-			./src/omp-skyline < $datafile > $parallel
-		else
-			parallel="./results/mpi-${temp}"
-			./src/mpi-skyline < $datafile > $parallel
-		fi
-		if diff $serial $parallel; then
-			echo -e "\nresults match!\n"
-		       	((correct++))
-		else
-			echo -e "\nresults don't match!\n"
-			((errors++))
-		fi	
+	echo "computing $datafile.."
+	temp="${datafile##*/}"
+	temp="${temp::-2}out"
+	serial="./results/serial-${temp}"
+	parallel="./results/${mode}-${temp}"
+	./src/skyline < ${datafile} > $serial
+	./src/${mode}-skyline < ${datafile} > $parallel
+	if diff ${serial} ${parallel}; then
+		echo -e "\nresults match!\n"
+		((correct++))
 	else
-		echo "$datafile doesn't  exits!"
-	fi
+		echo -e "\nresults don't match!\n"
+		((errors++))
+	fi	
 done
 
 #display goodbaye message
